476f5c2bf3b9fce613eeed70e02e9d7da17d4644
Igor Ghignatti <igor.ghignatti@gmail.com>
2024-02-25 17:14:23 -0300

    put base url back

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://igorghi.github.io',
-    // base: '/blog',
+    base: '/blog',
     integrations: [mdx(), sitemap()],
 });
